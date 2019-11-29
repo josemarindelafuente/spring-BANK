6ba3875309894fcb676edec284b7dafd570c466a
jose Marin <jose.marindelafuente@gmai.com>
2019-11-29 11:17:19 -0300

    se agrego el fixed-top del navbar con una solucion en la hoja de estilo para acomodoar el contenido del body

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import '../css/style.css';
-//import './clientes';
+import { Login } from './Login';
 
 
 
@@ -11,20 +11,22 @@ import '../css/style.css';
 
 /* evento click en boton login de usuarios*/ 
 var boton_form_login = document.getElementById('boton_form_login');
+
 boton_form_login.addEventListener("click", function (event) { 
 
     event.preventDefault();
 
     let var_login_user = document.querySelector("#var_login_user").value;
     let var_login_password = document.querySelector("#var_login_password").value;
-    
-    console.log("hiciste click en el boton de login> " + var_login_user + " " + var_login_password );
+
+    Login.loginUsuario("var_login_user", "var_login_password");
+
 
 
     if ( var_login_user === "admin" && var_login_password === "1234") {
          console.log ('ustes es admin');
          sessionStorage.setItem("usuarioActivo", "admin");
-         window.location.href = "admin.html";
+         window.location.href = "admin.html";         
     } else {
         console.log ('ustes NO es admin');
 
---
+++ src/js/login.js
@@ -0,0 +1,26 @@
+console.log ('este es login.js');
+
+export class Login {
+
+    constructor(){
+
+    }
+
+
+
+static loginUsuario (username, passwprd) {
+return console.log ("ejecutando metodo de clase" + " " + username + " " + passwprd );
+}
+
+
+
+
+
+
+
+
+
+
+
+    
+}
